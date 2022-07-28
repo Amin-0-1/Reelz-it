488f6d65a79a79b1a594f4cde602cb4908b95e39
Amin <mahmoudamin373@gmail.com>
2022-07-28 22:36:37 +0200

    prepare structure
    * structure created

--- Reelz'it/Features/Home/presetation/View/HomeVC.swift
+++ Reelz'it/Features/Home/presetation/View/HomeVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeVC.swift
 //  Reelz'it
 //
 //  Created by Amin on 28/07/2022.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
-
 }
-
